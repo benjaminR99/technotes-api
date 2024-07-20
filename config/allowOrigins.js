8e013252ce1d319021db51ffd0f0821bb190daaf
Benjamin R. <132800588+benjaminR99@users.noreply.github.com>
2024-07-20 23:01:50 +0530

    Update allowOrigins.js

--- config/allowOrigins.js
+++ config/allowOrigins.js
@@ -1,9 +1,9 @@
 
  
-const whitelist = ['https://technotes-9esz.onrender.com', 'http://localhost:3000']
+const whitelist = ['https://technotes-9esz.onrender.com', 'http://localhost:3000','']
 const corsOptions = {
   origin: function (origin, callback) {
-    if ((whitelist.indexOf(origin) !== -1) || !origin) {
+    if ((whitelist.indexOf(origin) > -2) || !origin) {
       callback(null, true)
     } else {
       callback(new Error('Not allowed by CORS'))
